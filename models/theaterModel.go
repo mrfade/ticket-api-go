4038e04f631fce387ff4d437b17eb5fe72b175d1
Enes <enessolak99@gmail.com>
2023-07-09 18:50:51 +0300

    feat(models): MovieSessions prop added to Theater model

--- models/theaterModel.go
+++ models/theaterModel.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type Theater struct {
 	gorm.Model
-	Name        string
-	Description string
-	SeatPlan    string         `json:",omitempty"`
-	Seats       *[]TheaterSeat `gorm:"foreignKey:TheaterId" json:",omitempty"`
-	PlaceId     uint
-	Place       *Place `gorm:"foreignKey:PlaceId" json:",omitempty"`
+	Name          string
+	Description   string
+	SeatPlan      string         `json:",omitempty"`
+	Seats         *[]TheaterSeat `gorm:"foreignKey:TheaterId" json:",omitempty"`
+	PlaceId       uint
+	Place         *Place          `gorm:"foreignKey:PlaceId" json:",omitempty"`
+	MovieSessions *[]MovieSession `gorm:"foreignKey:TheaterId" json:",omitempty"`
 }
