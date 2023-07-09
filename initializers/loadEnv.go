b4ad75336b366e3b69c859b93f1bc572c262babd
Enes Solak <enes@solak.dev>
2023-07-10 00:00:18 +0300

    fix(loadenv): do not log if its release mode

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -3,12 +3,13 @@ package initializers
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
 	err := godotenv.Load()
-	if err != nil {
+	if err != nil && gin.Mode() != "release" {
 		log.Fatal("Error loading .env file")
 	}
 }
