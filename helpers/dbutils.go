36522238c63fb5f34cb9fc2a33b8d0445c5f9cc0
Enes <enessolak99@gmail.com>
2023-07-09 18:07:21 +0300

    feat(helpers): database utils added, FirstOrFail, FirstOrFailWithSlug

---
+++ helpers/dbutils.go
@@ -0,0 +1,48 @@
+package helpers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mrfade/ticket-api-go/initializers"
+)
+
+func FirstOrFail(c *gin.Context, model interface{}, id string, preload ...string) error {
+	var query = initializers.DB
+
+	for _, v := range preload {
+		query = query.Preload(v)
+	}
+
+	query.First(model, id)
+
+	if query.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Not found!",
+		})
+
+		return query.Error
+	}
+
+	return nil
+}
+
+func FirstOrFailWithSlug(c *gin.Context, model interface{}, id string, preload ...string) error {
+	var query = initializers.DB
+
+	for _, v := range preload {
+		query = query.Preload(v)
+	}
+
+	query.Where("id = ?", id).Or("slug = ?", id).First(model)
+
+	if query.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Not found!",
+		})
+
+		return query.Error
+	}
+
+	return nil
+}
