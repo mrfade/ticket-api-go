95056c5fff3740265936cf55e0f95d89d484b01f
Enes Solak <enes@solak.dev>
2023-07-11 23:20:38 +0300

    fix(models): hide password field on userModel

--- models/userModel.go
+++ models/userModel.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name     string
-	Email    string `gorm:"unique"`
-	Password string
+	Email    string  `gorm:"unique"`
+	Password string  `json:"-"`
 	Status   string  `gorm:"default:active"`
 	Roles    *[]Role `gorm:"many2many:user_roles;"`
 }
